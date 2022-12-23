aa470a534f33e8a52d0366d195de132d7525c18a
timow-gh <39085531+timow-gh@users.noreply.github.com>
2022-12-23 17:48:28 +0100

    Added output dir option.

--- src/zotero_to_file_tree_src/main.cpp
+++ src/zotero_to_file_tree_src/main.cpp
@@ -11,9 +11,8 @@ namespace zotfiles
 {
 /*
  * TODO:
- * - add a command line option to specify the output directory
- * - add a command line option to specify the collection name (optional)
  * - add sql statement to get all std::filesystem paths of items with a pdf attachment
+ * - add a command line option to specify the collection name (optional)
  * - add sql statement to get all std::filesystem paths of items with a pdf attachment in a specific collection
  * - add sql statement to get all std::filesystem paths of items with a pdf attachment in a specific collection and its subcollections
  * - Build a tree of the collection hierarchy, each node has the name of the collection and a list of std::filesystem paths to the pdfs
@@ -24,17 +23,8 @@ namespace zotfiles
 
 static constexpr std::string_view zotero_db_name = "zotero.sqlite";
 
-} // namespace zotfiles
-
-int main(int argc, char** argv)
+std::filesystem::path get_zotero_db_path(const std::string& library_path_str)
 {
-  CLI::App app{"Converts a exported zotero library to a file tree."};
-
-  std::string library_path_str{""};
-  app.add_option("--zotero_dir", library_path_str, "Path to the zotero library. Default is the current directory.");
-
-  CLI11_PARSE(app, argc, argv);
-
   std::filesystem::path zotero_lib_path = std::filesystem::path(library_path_str);
   if (!std::filesystem::exists(zotero_lib_path))
   {
@@ -46,18 +36,46 @@ int main(int argc, char** argv)
     zotero_lib_path /= zotfiles::zotero_db_name;
   }
 
-  if (!std::filesystem::exists(zotero_lib_path) || !std::filesystem::is_regular_file(zotero_lib_path))
+  return zotero_lib_path;
+}
+
+} // namespace zotfiles
+
+int main(int argc, char** argv)
+{
+  CLI::App app{"Converts a exported zotero library to a file tree."};
+
+  std::string libraryPathStr{""};
+  app.add_option("-l,--lib_path", libraryPathStr, "Path to the zotero library. Default is the current directory.");
+
+  std::string outputDirStr{""};
+  app.add_option("-o,--output_dir", outputDirStr, "Path to the output directory.")->required();
+
+  CLI11_PARSE(app, argc, argv);
+
+  auto zoteroDbPath = zotfiles::get_zotero_db_path(libraryPathStr);
+  if (!std::filesystem::exists(zoteroDbPath))
   {
-    fmt::print("The zotero library path is invalid: {}\n", zotero_lib_path.string());
+    fmt::print("The zotero library path is invalid: {}\n", zoteroDbPath.string());
     return EXIT_FAILURE;
   }
 
-  if (!zotfiles::is_supported_zotero_db(zotero_lib_path))
+  auto outputDirPath = std::filesystem::path(outputDirStr);
+  if (std::filesystem::exists(outputDirPath))
+  {
+    fmt::print("The output directory already exists: {}\n", outputDirPath.string());
+    return EXIT_FAILURE;
+  }
+
+  // TODO: std::filesystem::create_directories(outputDirPath);
+  // TODO: read items with pdf attachement form db
+
+  if (!zotfiles::is_supported_zotero_db(zoteroDbPath))
   {
     return EXIT_FAILURE;
   }
 
-  zotfiles::ZoteroDBInfo zoteroDBInfo = zotfiles::zotero_db_info(zotero_lib_path);
+  zotfiles::ZoteroDBInfo zoteroDBInfo = zotfiles::zotero_db_info(zoteroDbPath);
   std::stringstream ss;
   ss << zoteroDBInfo;
   std::cout << ss.str();
